eb9f0714cf9b07cc79d101eed1695e8247755588
Hubert Guzowski <hubert.guzowski@gmail.com>
2021-03-28 18:21:24 +0200

    Added data loading utils and simple view for them

--- bankruptcy_model/views.py
+++ bankruptcy_model/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from bankruptcy_model.utils import data_loading
+
+
+def dataframe_view(request, year_number):
+    dataframe = data_loading.load_dataset_by_year(year_number)
+    return HttpResponse(dataframe.to_html())
