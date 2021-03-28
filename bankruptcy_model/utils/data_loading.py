0e3032fe3f8823be582be54ecc9856bba66285c0
Hubert Guzowski <hubert.guzowski@gmail.com>
2021-03-28 18:23:27 +0200

    Cleanup

--- bankruptcy_model/utils/data_loading.py
+++ bankruptcy_model/utils/data_loading.py
@@ -7,6 +7,6 @@ from bankruptcy_prediction.settings import BASE_DIR
 
 
 def load_dataset_by_year(year_number) -> pd.DataFrame:
-    file_path = os.path.join(BASE_DIR, 'data', str(year_number)+'year.arff')
+    file_path = os.path.join(BASE_DIR, 'data', str(year_number) + 'year.arff')
     data = arff.loadarff(file_path)
     return pd.DataFrame(data[0])
